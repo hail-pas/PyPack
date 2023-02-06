0f6490078b607630290b6a5f6bd0ba706c010541
panhq <hypofiasco@gmail.com>
2023-02-06 18:24:09 +0800

    add getHbaseService method

--- src/main/java/cn/burnish/Main.java
+++ src/main/java/cn/burnish/Main.java
@@ -5,12 +5,16 @@ import cn.burnish.hbase.HbaseService;
 import py4j.GatewayServer;
 
 public class Main {
-    public final HbaseService hbaseService;
+    private final HbaseService hbaseService;
 
     public Main() {
         Config conf = new Config();
         conf.loadConfig();
         this.hbaseService = new HbaseService(conf);
+    }
+
+    public HbaseService getHbaseService() {
+        return hbaseService;
     }
 
     public static void main(String[] args) {
